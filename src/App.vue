556abd4238bbf9a6d0f56187e4660ce47b4a353a
yangpanda <gnaymuyi@163.com>
2021-07-31 21:37:23 +0800

    将 header 代码独立出去

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,7 @@
   <div class="wrapper">
     <n-layout position="absolute">
       <n-layout-header class="header">
-        <router-link class="home-link" to="/">
-          <svg-icon
-            iconName="#icon-logo"
-            iconColor="#ec4141"
-            container
-            style="margin-right: 5px"
-          />
-          <span class="title">网易云音乐</span>
-        </router-link>
-        <login />
+        <the-header />
       </n-layout-header>
       <n-layout class="center" has-sider position="absolute">
         <n-layout-sider bordered :native-scrollbar="false">
@@ -29,8 +20,8 @@
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
 import Sidebar from "@/components/Sidebar.vue";
-import Login from "@/components/Login.vue";
 import Player from "@/components/Player.vue";
 
 import {
@@ -43,6 +34,7 @@ import {
 
 export default {
   components: {
+    TheHeader,
     Sidebar,
     Player,
     NLayout,
@@ -50,7 +42,6 @@ export default {
     NLayoutSider,
     NLayoutContent,
     NLayoutFooter,
-    Login,
   },
 };
 </script>
@@ -58,29 +49,6 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
   height: 100%;
-  position: relative;
-
-  .header {
-    height: var(--header-height);
-    padding: 0 20px;
-    background-color: var(--red-color);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    .home-link {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      font-size: 18px;
-      color: #fff;
-      text-decoration: none;
-
-      .logo {
-        margin-right: 5px;
-      }
-    }
-  }
 
   .center {
     top: var(--header-height);
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <form>
+      <input type="search" placeholder="搜索">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  outline: none;
+}
+</style>
