72c1faf01e4a0cd677ee91d46ff0a5730648807d
yangpanda <gnaymuyi@163.com>
2021-12-20 16:53:55 +0800

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  printWidth: 110,
+  tabWidth: 2,
+  useTabs: false, // 不使用 tab indent
+  semi: true, // 行尾添加分号
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  insertPragma: false,
+  requirePragma: false,
+};
