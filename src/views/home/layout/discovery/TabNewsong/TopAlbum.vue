da859b41aa7e0eac3d7654dce9cda965f452f1b8
yangpanda <gnaymuyi@163.com>
2021-12-25 16:07:13 +0800

    重构

--- src/views/home/layout/discovery/TabNewsong/TopAlbum.vue
+++ src/views/home/layout/discovery/TabNewsong/TopAlbum.vue
@@ -3,7 +3,7 @@
     <div :class="$style.header">
       <div :class="$style.nav">
         <div
-          v-for="(value, name) in Object.fromEntries(albumAreaMap)"
+          v-for="(value, name) in Object.fromEntries(areaMap)"
           :key="value"
           @click="area = value"
           :class="['cursor-pointer', $style.navItem, area == value ? $style.active : '']"
@@ -13,7 +13,7 @@
       </div>
       <div :class="$style.nav">
         <div
-          v-for="(value, name) in Object.fromEntries(albumTypeMap)"
+          v-for="(value, name) in Object.fromEntries(typeMap)"
           :key="value"
           @click="type = value"
           :class="['cursor-pointer', $style.navItem, type == value ? $style.active : '']"
@@ -22,79 +22,86 @@
         </div>
       </div>
     </div>
-    <n-grid v-if="weekAlbums" x-gap="20" y-gap="20" :cols="5">
-      <n-grid-item v-for="(item, index) in weekAlbums" :key="index">
-        <div class="album-card pointer" @click="toPlaylistDetail(item.id)">
+    <div v-if="loading" :class="$style.spinContainer">
+      <n-spin></n-spin>
+    </div>
+    <n-grid v-else x-gap="20" y-gap="20" :cols="5">
+      <n-grid-item v-for="(item, index) in monthAlbums" :key="item.id">
+        <div :class="$style.albumCard" @click="toAlbumDetail(item.id)">
           <the-image :src="item.picUrl + '?param=200y200'" round="large" />
-          <div class="album-name">{{ item.name }}</div>
-        </div>
-      </n-grid-item>
-    </n-grid>
-    <n-grid x-gap="20" y-gap="20" :cols="5">
-      <n-grid-item v-for="(item, index) in monthAlbums" :key="index">
-        <div class="album-card pointer" @click="toPlaylistDetail(item.id)">
-          <the-image :src="item.picUrl + '?param=200y200'" round="large" />
-          <div class="album-name">{{ item.name }}</div>
+          <div>{{ item.name }}</div>
         </div>
       </n-grid-item>
     </n-grid>
   </div>
 </template>
 
-<script setup>
-import { NGrid, NGridItem } from 'naive-ui';
+<script>
+import { NGrid, NGridItem, NSpin } from 'naive-ui';
+import { ref, toRefs, watchEffect, reactive } from '@vue/runtime-core';
+import useRouterMethods from '@/composables/router-methods';
+import api from '@/api';
 
-import { ref, watchEffect, reactive } from '@vue/runtime-core';
-import { useRouter } from 'vue-router';
-import * as playlist from '@/api/service/playlist.js';
-
-const router = useRouter();
-const showSpin = ref(true);
-
-const albumAreaMap = new Map([
+const areaMap = new Map([
   ['全部', 'ALL'],
   ['华语', 'ZH'],
   ['欧美', 'EA'],
   ['韩国', 'KR'],
   ['日本', 'JP'],
 ]);
-const albumTypeMap = new Map([
+const typeMap = new Map([
   ['全部', 'new'],
   ['热门', 'hot'],
 ]);
-const area = ref('ALL');
-const type = ref('new');
-const year = ref(new Date().getFullYear());
-const month = ref(new Date().getMonth() + 1);
-const offset = ref(0);
-const limit = ref(30);
 
-const weekAlbums = ref([]);
-const monthAlbums = ref([]);
+export default {
+  name: 'TopAlbum',
+  components: {
+    NGrid,
+    NGridItem,
+    NSpin,
+  },
+  setup() {
+    const loading = ref(true);
+    const monthAlbums = ref([]);
+    const state = reactive({
+      area: 'ALL',
+      type: 'new',
+      year: new Date().getFullYear(),
+      month: new Date().getMonth() + 1,
+    });
 
-const getTopAlbum = async (params) => {
-  playlist.getTopAlbum(params).then((res) => {
-    if (res.code === 200) {
-      weekAlbums.value = res.weekData;
-      monthAlbums.value = res.monthData;
-    }
-  });
+    const { toAlbumDetail } = useRouterMethods();
+
+    const getTopAlbum = async (params) => {
+      loading.value = true;
+      const res = await api.playlist.getTopAlbum(params);
+      if (res.code === 200) {
+        console.log(res);
+        monthAlbums.value = res.monthData;
+        loading.value = false;
+      }
+    };
+
+    watchEffect(() => {
+      getTopAlbum({
+        area: state.area,
+        type: state.type,
+        year: state.year,
+        month: state.month,
+      });
+    });
+
+    return {
+      loading,
+      monthAlbums,
+      areaMap,
+      typeMap,
+      toAlbumDetail,
+      ...toRefs(state),
+    };
+  },
 };
-
-watchEffect(() => {
-  getTopAlbum({
-    area: area.value,
-    type: type.value,
-    year: year.value,
-    month: month.value,
-    offset: offset.value,
-    limit: limit.value,
-  });
-});
-
-function toPlaylistDetail(id) {
-  router.push(`/album-detail/${id}`);
-}
 </script>
 
 <style module>
@@ -113,5 +120,13 @@ function toPlaylistDetail(id) {
 }
 .active {
   color: green;
+}
+.spinContainer {
+  margin: 20px 0;
+  display: flex;
+  justify-content: center;
+}
+.albumCard {
+  cursor: pointer;
 }
 </style>
